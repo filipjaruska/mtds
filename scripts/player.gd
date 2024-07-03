fea60f96c36c22e415986e8babe6c2aa3d5d8826
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-07-03 17:02:39 +0200

    weird camera bug which wasn't even a thing before

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
-var sync_position  = Vector2(0,0) #alternative way to synchorinze movement, not currently used
+var sync_position = Vector2(0, 0) # Alternative way to synchronize movement, not currently used
 @onready var weapon_manager = $WeaponManager
 
 func _ready():
 	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		$Camera2D.enabled = true
+	else:
+		$Camera2D.enabled = false
 
 func _process(delta):
 	if $MultiplayerSynchronizer.get_multiplayer_authority() != multiplayer.get_unique_id():
-		global_position = global_position.lerp(sync_position , 0.2)
+		global_position = global_position.lerp(sync_position, 0.2)
 	
-	sync_position  = global_position
+	sync_position = global_position
 	
 	var input_vector = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
@@ -21,7 +25,7 @@ func _process(delta):
 	move_and_slide()
 	weapon_manager._process(delta)
 	
-func _physics_process(delta):
+func _physics_process(_delta):
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
 		var direction = (get_global_mouse_position() - global_position).normalized()
 		rotation = direction.angle()
