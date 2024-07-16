dc581fbad966c28224e9945a76c8bd1bfd9b9356
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-07-16 13:43:13 +0200

    require pressing e to pickup weapon

--- scripts/weapons/weapon_pickup.gd
+++ scripts/weapons/weapon_pickup.gd
@@ -1,13 +1,26 @@
 extends Area2D
 
 @export var weapon_scene: PackedScene
+var player = null
+
 
 func _ready() -> void:
 	$Sprite2D.visible = false
-	var idkfuckingknowanymorewhatthefuckwasievendoingwhytrytousesignalswhenthierdocumentationsucksassandimnotafanofthemanyway: Node = weapon_scene.instantiate()
-	add_child(idkfuckingknowanymorewhatthefuckwasievendoingwhytrytousesignalswhenthierdocumentationsucksassandimnotafanofthemanyway)
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		body.weapon_manager.on_weapon_picked_up(weapon_scene)
+	add_child(weapon_scene.instantiate())
+	
+func _process(delta):
+	if player != null and Input.is_action_just_pressed("interact"):
+		player.weapon_manager.on_weapon_picked_up(weapon_scene)
 		queue_free()
+		# show something to indicate to the player how to pick it up or something
+	pass
+	
+func _on_body_entered(body):
+	if not body.is_in_group("Player"):
+		return
+	player = body
+
+func _on_body_exited(body):
+	if not body.is_in_group("Player"):
+		return
+	player = null
