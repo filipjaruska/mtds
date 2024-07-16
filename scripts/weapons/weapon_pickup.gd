30b5f538dd63534eee19c98569146ba23f07a10c
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-07-16 13:53:41 +0200

    add visual indication for pickup

--- scripts/weapons/weapon_pickup.gd
+++ scripts/weapons/weapon_pickup.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @export var weapon_scene: PackedScene
+@onready var label = $Label
 var player = null
+var ref
 
 
 func _ready() -> void:
+	label.visible = false
 	$Sprite2D.visible = false
 	add_child(weapon_scene.instantiate())
 	
@@ -12,15 +15,15 @@ func _process(delta):
 	if player != null and Input.is_action_just_pressed("interact"):
 		player.weapon_manager.on_weapon_picked_up(weapon_scene)
 		queue_free()
-		# show something to indicate to the player how to pick it up or something
-	pass
 	
 func _on_body_entered(body):
 	if not body.is_in_group("Player"):
 		return
 	player = body
+	label.visible = true
 
 func _on_body_exited(body):
 	if not body.is_in_group("Player"):
 		return
 	player = null
+	label.visible = false
