43b3def15b65b44dc8caae405936e96eb900e1e5
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-12-16 11:43:19 +0100

    Move muzzle_flash.tscn to weapons folder

--- scripts/weapons/ranged_weapon.gd
+++ scripts/weapons/ranged_weapon.gd
@@ -17,7 +17,7 @@ class_name RangedWeapon
 @export var sprite = Sprite2D # weapon sprite
 
 @onready var bullet_scene = preload("res://nodes/weapons/bullet.tscn")
-@onready var muzzle_flash = preload("res://nodes/scenes/muzzle_flash.tscn") 
+@onready var muzzle_flash = preload("res://nodes/weapons/muzzle_flash.tscn") 
 @export var animation_player: AnimationPlayer
 
 var last_shot_time: float = 0.0
