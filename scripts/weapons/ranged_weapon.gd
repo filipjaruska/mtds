086002da55402e430ad8acb2c0df36c7fc5596f7
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2025-02-03 12:42:05 +0100

    Rework bullets to be raycast

--- scripts/weapons/ranged_weapon.gd
+++ scripts/weapons/ranged_weapon.gd
@@ -40,16 +40,20 @@ func shoot():
 func _shoot_bullet():
 	for i in range(pellets):
 		if bullet_scene:
-			var bullet: Area2D = bullet_scene.instantiate()
-			get_parent().get_parent().get_parent().add_child(bullet)
+			var bullet = bullet_scene.instantiate()
+			
+			var spawn_pos = muzzle.global_position
+			var deviation = (1.0 - accuracy / 100.0) * 0.5
+			var shot_rotation = global_rotation + randf() * deviation - deviation / 2
+			
+			get_tree().root.add_child(bullet)
+			
+			bullet.global_position = spawn_pos
+			bullet.global_rotation = shot_rotation
+			bullet.target_position = Vector2(max_range, 0)
+			bullet.set_visual_range(max_range)
 			bullet.set_bullet_damage(damage, armor_penetration)
-			bullet.set_bullet_lifetime(max_range / bullet.SPEED)
-			
-			var offset: Vector2 = Vector2(max_range / 10, 0).rotated(global_rotation)
-			bullet.global_position = muzzle.global_position + offset
-			
-			var deviation: float = (1.0 - accuracy / 100.0) * 0.5 # deviation based on accuracy
-			bullet.rotation = global_rotation + randf() * deviation - deviation / 2
+			bullet.force_raycast_update()
 
 func reload():
 	if is_reloading:
